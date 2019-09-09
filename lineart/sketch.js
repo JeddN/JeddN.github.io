09554f1a63ae6539ca537b3f15a73526ff58907e
JeddN <45491547+JeddN@users.noreply.github.com>
2019-09-09 14:11:59 -0600

    lineart

---
+++ lineart/sketch.js
@@ -0,0 +1,17 @@
+// Line Art Demo
+// Agnes
+// September 9, 2019
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+}
+
+function draw() {
+ line(mouseX,mouseY,pmouseX,pmouseY);
+}
