ec0a354fe6bebd684ff7dfab720ef3fd5232b0b7
JeddN <45491547+JeddN@users.noreply.github.com>
2019-09-18 14:11:37 -0600

    work

--- catchtheball/sketch.js
+++ catchtheball/sketch.js
@@ -1,2 +1,25 @@
 let x;
 let y;
+let scalar = 2 
+
+function setup(){
+    createCanvas(width, height);
+}
+
+function draw(){
+    background(255);
+    
+    square(20,30,50);
+    fill(0)
+    
+    square(x,y)
+    if (keyPressed){
+    
+        if (keyCode === UP_ARROW){
+        scalar *= 2.5;
+        }
+        if (keyCode === DOWN_ARROW){
+        scalar /=2.5;
+    }
+}
+}
